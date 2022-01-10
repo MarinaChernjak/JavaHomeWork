836cd2dfd23f3e96ab7fd5a9ca56ce66f5ff8286
MarinaChernjak <marina.chernjak@privatbank.ua>
2022-01-10 23:03:29 +0200

    add Reader/Writer threads synchronization

--- src/com/pb/chernjak/hw13/ReaderWriter.java
+++ src/com/pb/chernjak/hw13/ReaderWriter.java
@@ -5,49 +5,62 @@ import java.util.Queue;
 
 public class ReaderWriter {
 
-    public static final int MAX_QUEUE_SIZE = 5;
+    public static final int MAX_QUEUE_SIZE = 3;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Queue<Integer> queue = new LinkedList<>();
 
-        Writer writer = new Writer(queue);
+        Writer writer = new Writer(queue, MAX_QUEUE_SIZE);
         Thread writerThread = new Thread(writer);
         writerThread.start();
 
         Reader reader = new Reader(queue);
         Thread readerThread = new Thread(reader);
         readerThread.start();
+
+        Thread.sleep(10);
+        writerThread.interrupt();
+        readerThread.interrupt();
     }
 
     private static class Writer implements Runnable {
-        private Queue<Integer> queue;
+        private final Queue<Integer> queue;
+        int maxSize;
 
-        public Writer(Queue<Integer> queue) {
+        public Writer(Queue<Integer> queue, int maxSize) {
             this.queue = queue;
+            this.maxSize = maxSize;
         }
 
         @Override
         public void run() {
             int number = 0;
-            while (true) {
-                if (queue.size() <= MAX_QUEUE_SIZE) {
-                    System.out.println("Записать число: " + number);
-                    queue.offer(number);
-                    number++;
-                    System.out.println("Размер очереди: " + queue.size());
-                }
+            while (!Thread.interrupted()) {
                 try {
-                    Thread.sleep(500 + (int)(Math.random() * 2000));
+                    System.out.println("Записать число: " + number);
+                    write(number++);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                    break;
                 }
+            }
+        }
+
+        public void write(int number) throws InterruptedException {
+            synchronized (queue) {
+                if (queue.size() >= maxSize) {
+                    System.out.println("Ожидание записи ...");
+                    queue.wait();
+                }
+                queue.offer(number);
+                queue.notifyAll();
             }
         }
     }
 
     private static class Reader implements Runnable {
-        private Queue<Integer> queue;
+        private final Queue<Integer> queue;
 
         public Reader(Queue<Integer> queue) {
             this.queue = queue;
@@ -55,16 +68,23 @@ public class ReaderWriter {
 
         @Override
         public void run() {
-            while (true) {
-                if (queue.size() > 0) {
-                    int number = queue.poll();
-                    System.out.println("Считать число: " + number);
-                }
+            while (!Thread.interrupted()) {
                 try {
-                    Thread.sleep(500 + (int)(Math.random() * 2000));
+                    System.out.println("Считать число: " + read());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+        }
+
+        public int read() throws InterruptedException {
+            synchronized (queue) {
+                if (queue.isEmpty()) {
+                    System.out.println("Ожидание чтения ...");
+                    queue.wait();
+                }
+                queue.notifyAll();
+                return queue.poll();
             }
         }
     }
